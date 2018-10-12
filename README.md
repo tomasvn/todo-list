25fb7465d345bfb7e8fb0b0786e3e075723d697f
Ngoc Tu Nguyen <nguyenngoct@gmail.com>
2018-10-12 18:40:58 +0200

    Initial Commit :sparkles:

---
+++ README.md
@@ -0,0 +1,9 @@
+# Todo List
+
+## About
+
+Created a todo list app, utilizing DOM manipulations, listening for events
+
+## TODO
+
+[ ] Add basic styling
---
+++ index.js
@@ -0,0 +1,36 @@
+/*document.addEventListener('keyup', keyFn)
+
+var keyFn = function (ev) {
+    if (ev.preventDefault()) return false
+
+    var key = ev.key || ev.keyCode
+
+    if (key === 'Enter' || key === 13) {
+
+    }
+}*/
+
+var btnAdd = document.querySelector('.jsAdd')
+var btnRemove = document.querySelector('.jsRemove')
+
+var add = function () {
+    var newElem = document.createElement('li') // We need to pass parameter, what kind of element we want to create
+    var list = document.querySelector('.jsList')
+    var inputValue = document.querySelector('.jsInput').value
+    var btnElem = document.createElement('button')
+
+    btnElem.textContent = 'Delete Me!'
+    btnElem.classList.add('btn')
+    btnElem.classList.add('jsRemove')
+    newElem.textContent = inputValue // Insert text
+    newElem.classList.add('elem')
+
+    list.appendChild(newElem) // Specify where do I want to append (inject) new element to, in "()" specify what child I want to inject
+    newElem.appendChild(btnElem)
+
+    btnElem.addEventListener('click', function () {
+        newElem.parentNode.removeChild(newElem)
+    })
+}
+
+btnAdd.addEventListener('click', add)
